80837fe7167be0ac9e9dff24a5c93855534f13f5
Michael-Susu12138 <78223776+Michael-Susu12138@users.noreply.github.com>
2023-04-24 15:59:09 -0400

    uppdate

---
+++ Assets/Code/ConstructSites.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructSites : MonoBehaviour
+{
+    Transform card;
+    Camera cam;
+    public LayerMask  UILayer;
+    public GameObject gamePanel;
+
+    private void Start() {
+        cam = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+# if UNITY_STANDALONE || UNITY_EDITOR
+
+        if(Input.GetMouseButtonDown(0)){
+            GameObject buildPanel = GameObject.FindGameObjectWithTag("build_panel");
+            BuildPanel(Input.mousePosition,buildPanel);
+        }
+
+# endif
+
+
+    }
+
+    void BuildPanel(Vector2 touchPos, GameObject buildPanel){
+        Vector2 worldPos = cam.ScreenToWorldPoint(touchPos);
+        if (Physics2D.OverlapPoint(worldPos, UILayer))
+        {
+            print("HIT");
+            
+            card = Physics2D.OverlapPoint(worldPos, UILayer).transform;
+            // if(card.gameObject.CompareTag)
+            //  using compare tag to check, put everything onto the UILayer
+            Instantiate(gamePanel,card.position,Quaternion.identity);
+        } 
+        // else if (buildPanel!=null){
+        //     print("not null");
+        //     print(buildPanelLayer);
+        //     print(Physics2D.OverlapPoint(worldPos,buildPanelLayer));
+        // } 
+        // else if (buildPanel!=null && Physics2D.OverlapPoint(worldPos,buildPanelLayer)){
+        //     print("Build a house");
+        // } 
+        else {
+            if(buildPanel != null){
+                Destroy(buildPanel);
+            }
+        }
+        
+         
+        
+
+    }
+}
---
+++ Assets/Code/FighterBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO:
+// text on health bar
+// health bar in canvas
+// canvas top left
+
+
+
+public class FighterBehavior : MonoBehaviour
+{
+    // Rigidbody2D _rigidbody2D;
+    // GameObject[] Enemies;
+    
+    
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     _rigidbody2D = 
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
