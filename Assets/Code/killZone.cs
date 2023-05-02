46080882d63b749ed4efb370ad9678df76db5767
Yilin Zhao <zy2280@nyu.edu>
2023-05-02 13:27:49 -0400

    ADD killZone prefab

---
+++ Assets/Code/killZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class killZone : MonoBehaviour
+{
+    public string levelToLoad;
+    public string[] tagsToDestroy;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(tagsToDestroy.Contains(other.tag)) {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
